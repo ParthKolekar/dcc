3731094b99eedc076326d0e760395b89ebb329ff
Parth Kolekar <parth.kolekar@students.iiit.ac.in>
2015-09-05 17:40:39 +0530

    Removed parsing from stdin.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@ extern "C" int yyparse();
 
 int main (const int argc, const char ** argv) {
     if (argc < 2) {
-        yyin = stdin;
+        yyin = NULL;
+        std::cerr <<"fatal error: no input files" << std::endl;
+        return -1;
     } else {
         FILE *infile = fopen(argv[1], "r");
         if (!infile) {
