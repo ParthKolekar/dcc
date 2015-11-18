b08ef532637eb1c07582eba0be234c5f4deb2f9c
Shrenik <shrenik.jain@research.iiit.ac.in>
2015-11-18 06:27:17 +0530

    PrintVisitor completed(beautification needed)

--- PrintVisitor.cpp
+++ PrintVisitor.cpp
@@ -10,29 +10,30 @@ class PrintVisitor : public Visitor
 {
 public:
     void visit(ASTProgram * node) {
-        std::cout<<"<program>"<<std::endl;
+        // std::cout<<"<program>"<<std::endl;
         if(node->getFdl() == NULL){
-            std::cout<<"<field_declarations count = \"0\">"<<std::endl;
+            // std::cout<<"<field_declarations count = \"0\">"<<std::endl;
         }
 
         else{
-            std::cout<<"<field_declarations count =\" "<<(node->getFdl())->size()<<">"<<std::endl;
+            // std::cout<<"<field_declarations count =\" "<<(node->getFdl())->size()<<"\">"<<std::endl;
             for(auto it = (node->getFdl())->begin() ; it != (node->getFdl())->end(); it++) {
                 (*it)->accept(this);
             }               
         }
-        std::cout<<"</method_declarations>"<<std::endl;
+        // std::cout<<"</field_declarations>"<<std::endl;
         if(node->getMdl() == NULL){
-            std::cout<<"<method_declarations count = \"0\">"<<std::endl;
+            // std::cout<<"<method_declarations count = \"0\">"<<std::endl;
         }
 
         else{
-            std::cout<<"<method_declarations count =\" "<<(node->getMdl())->size()<<">"<<std::endl;
+            // std::cout<<"<method_declarations count =\" "<<(node->getMdl())->size()<<"\">"<<std::endl;
             for(auto it = (node->getMdl())->begin() ; it != (node->getMdl())->end(); it++) {
                 (*it)->accept(this);
             }               
         }
-        std::cout<<"</method_declarations>"<<std::endl;
+        // std::cout<<"</method_declarations>"<<std::endl;
+        // std::cout<<"</program>"<<std::endl;
     }
     void visit(ASTFieldDecl * node) {
         std::cout<<node->parseDatatype(node->getType())<<" ";
@@ -57,95 +58,150 @@ public:
     }
 
     void visit(ASTMethodDecl * node) {
-        std::cout<<node->getId()<<" "<<node->parseDatatype(node->getReturnType())<<" ";
+        std::cout<<node->parseDatatype(node->getReturnType())<<" "<<node->getId()<<" ";
+        std::cout<<"(";
         if (node->getArguments()) {
             for(auto it = (node->getArguments())->begin() ; it != (node->getArguments())->end(); it++) {
                 (*it)->accept(this);
             }
         }
+        std::cout<<")"<<std::endl;
         /* Block statement is called*/
         node->getBlock()->accept(this);
     }
     void visit(ASTTypeIdentifier * node) {
-        std::cout<<node->getId()<<" "<<node->parseDatatype(node->getType())<<std::endl;
+        std::cout<<node->parseDatatype(node->getType())<<" "<<node->getId()<<" ";
     }
 
     void visit(ASTBlockStatement * node) {
         std::cout<<"{"<<std::endl;
+        // std::cout<<"no of statements = ";
+        if(node->getStmtlist() && node->getId_list()){
+            std::cout<<node->getStmtlist()->size() + node->getId_list()->size()<<std::endl;
+        for(auto it = (node->getId_list())->begin() ; it != (node->getId_list())->end(); it++) {
+            (*it)->accept(this);
+        }
         for(auto it = (node->getStmtlist())->begin() ; it != (node->getStmtlist())->end(); it++) {
             (*it)->accept(this);
         }
+        }
+        else if(node->getStmtlist()){
+            std::cout<<node->getStmtlist()->size()<<std::endl;
+            for(auto it = (node->getStmtlist())->begin() ; it != (node->getStmtlist())->end(); it++) {
+            (*it)->accept(this);
+        }
+        }
+        else if(node->getId_list()){
+            std::cout<<node->getId_list()->size()<<std::endl;
+            for(auto it = (node->getId_list())->begin() ; it != (node->getId_list())->end(); it++) {
+            (*it)->accept(this);
+        }
+        }
+        
+        std::cout<<std::endl;
+        
         std::cout<<"}"<<std::endl;
     }
 
     void visit(ASTAssignmentStatement * node) {
-        std::cout<<""<<std::endl;
+        node->getLocation()->accept(this);
+        std::cout<<" "<<node->parseAssignOp(node->getOp())<<" ";
+        node->getExpr()->accept(this);
+        std::cout<<std::endl;
     }
     void visit(ASTMethodCall * node) {
         std::cout<<""<<std::endl;
     }
     void visit(ASTNormalMethod * node) {
-        std::cout<<""<<std::endl;
+        std::cout<<node->getId()<<"(";
+        if(node->getArguments()){
+            for(auto it = (node->getArguments())->begin() ; it != (node->getArguments())->end(); it++) {
+              (*it)->accept(this);
+            }
+        }
+        std::cout<<")";
     }
     void visit(ASTCalloutMethod * node) {
-        std::cout<<""<<std::endl;
+        std::cout<<node->getMethod_name()<<"(";
+        if(node->getArguments()){
+            for(auto it = (node->getArguments())->begin() ; it != (node->getArguments())->end(); it++) {
+              (*it)->accept(this);
+            }
+        }
+        std::cout<<")"<<std::endl;
     }
     void visit(ASTCalloutArg * node) {
-        std::cout<<""<<std::endl;
+        
     }
     void visit(ASTStringCalloutArg * node) {
-        std::cout<<""<<std::endl;
+        std::cout<<node->getArgument();
     }
     void visit(ASTExpressionCalloutArg * node) {
-        std::cout<<""<<std::endl;
+        node->getArgument()->accept(this);
     }
     void visit(ASTIfStatement * node) {
-        std::cout<<""<<std::endl;
+        std::cout<<"if(";
+        node->getCondition()->accept(this);
+        std::cout<<")"<<std::endl;
+        node->getIf_block()->accept(this);
+        if(node->getElse_block())
+            node->getElse_block()->accept(this);
     }
     void visit(ASTForStatement * node) {
-        std::cout<<""<<std::endl;
+        node->getInit_condition()->accept(this);
+        node->getEnd_condition()->accept(this);
+        node->getBlock()->accept(this);
+
     }
     void visit(ASTReturnStatement * node) {
-        std::cout<<""<<std::endl;
+        std::cout<<"return ";
+        node->getReturn_expr()->accept(this);
     }
     void visit(ASTContinueStatement * node) {
-        std::cout<<""<<std::endl;
+        std::cout<<"continue"<<std::endl;
     }
     void visit(ASTBreakStatement * node) {
-        std::cout<<""<<std::endl;
+        std::cout<<"break"<<std::endl;
     }
     void visit(ASTLocation * node) {
         std::cout<<""<<std::endl;
     }
     void visit(ASTVarLocation * node) {
-        std::cout<<""<<std::endl;
+        std::cout<<node->getId()<<" ";
     }
     void visit(ASTArrayLocation * node) {
-        std::cout<<""<<std::endl;
+        std::cout<<node->getId()<<" ["<<node->getIndex()<<"]";
     }
     void visit(ASTLiteralExpression * node) {
         std::cout<<""<<std::endl;
     }
     void visit(ASTIntegerLiteralExpression * node) {
-        std::cout<<""<<std::endl;
+        std::cout<<" "<<node->getValue();
     }
     void visit(ASTCharLiteralExpression * node) {
-        std::cout<<""<<std::endl;
+        std::cout<<" "<<node->getValue();
     }
     void visit(ASTTrueLiteralExpression * node) {
-        std::cout<<""<<std::endl;
+        std::cout<<" "<<node->getValue();
     }
     void visit(ASTFalseLiteralExpression * node) {
-        std::cout<<""<<std::endl;
+        std::cout<<" "<<node->getValue();
     }
     void visit(ASTBinaryOperationExpression * node) {
-        std::cout<<""<<std::endl;
+        node->getLeft()->accept(this);
+        std::cout<<" "<<node->parseBinOp(node->getOp())<<" ";
+        node->getRight()->accept(this);
     }
     void visit(ASTUnaryOperationExpression * node) {
-        std::cout<<""<<std::endl;
+        std::cout<<node->parseUnOp(node->getOp())<<" "<<std::endl;
+        node->getExpr()->accept(this);
     }
     void visit(ASTVarDecl * node) {
-        std::cout<<""<<std::endl;
+        std::cout<<node->parseDatatype(node->getType())<<" ";
+        for(auto it = (node->getId_list())->begin() ; it != (node->getId_list())->end(); it++) {
+              (*it)->accept(this);
+            }
+        std::cout<<std::endl;
     }
     void visit(ASTIdentifier * node) {
         std::cout<<""<<std::endl;
