416d80d0cd9034b223166ff6c66aebf4ec4c4de8
Shrenik <shrenik.jain@research.iiit.ac.in>
2015-12-01 12:43:13 +0530

    Main method arguments handled

--- CodeGenVisitor.h
+++ CodeGenVisitor.h
@@ -78,6 +78,9 @@ public:
         if (node->getMdl()) {
             for(auto it = (node->getMdl())->begin() ; it != (node->getMdl())->end(); it++) {
                 iterator = static_cast<llvm::Function *>(this->visit(*it));
+                if ((*it)->getId() == "main" && (*it)->getArguments()->size() > 0) {
+                    return ErrorHandler("Main cannot have any arguments");
+                }
                 if ((*it)->getId() == "main" && !userMain) {
                     userMain = iterator;
                 }
