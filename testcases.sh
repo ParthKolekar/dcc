1067c095cbd8731f9e00410eb33be0203106ce52
Shrenik <shrenik.jain@research.iiit.ac.in>
2015-12-02 15:51:58 +0530

    updated testcases

--- testcases.sh
+++ testcases.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+make clean && make
 for i in `find \`pwd\` -name *.dcf`
 do
     echo
     echo $i
     ./dcc "$i" > tests/`basename $i`.ll
-    clang tests/`basename $i`.ll
+    clang tests/`basename $i`.ll -o tests/`basename $i`.out
+    ./tests/`basename $i`.out
     echo
 done
 
