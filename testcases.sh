e52a0ec10aff8732e7b8c3206714de2d3b0cc884
Parth Kolekar <parth.kolekar@students.iiit.ac.in>
2016-02-27 13:39:03 +0530

    Completed. :P. Ab Na Hoga.

--- testcases.sh
+++ testcases.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 make clean && make
+mkdir -p tests
 for i in `find \`pwd\` -name *.dcf`
 do
     echo
     echo $i
-    ./dcc "$i" > tests/`basename $i`.ll
-    clang tests/`basename $i`.ll -o tests/`basename $i`.out
-    ./tests/`basename $i`.out
+    ./dcc "$i" > tests/`basename $i .dcf`.ll
+    if [[ $? == 0 ]]
+    then
+        clang tests/`basename $i .dcf`.ll -o tests/`basename $i .dcf`.out
+        ./tests/`basename $i .dcf`.out
+    else
+        echo "Compiler Error"
+    fi
     echo
 done
 
